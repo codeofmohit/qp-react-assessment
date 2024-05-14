843b05f4d8631e6118d5f452f9a014744c5315a9
codeofmohit <mohit156@gmail.com>
2024-05-14 10:42:00 +0530

    bug fixed

--- todo-app/src/theme/Theme.tsx
+++ todo-app/src/theme/Theme.tsx
@@ -1,16 +1,11 @@
 import { useContext } from "react";
 import { ThemeContext } from "./themeContext";
-import { COLOR_SCHEME } from "../constants/constants";
 
 const Theme = ({ children }: { children: React.ReactElement }) => {
   const theme = useContext(ThemeContext);
   return (
     <div className={`theme ${theme.currentTheme}`}>
-      <div
-        className={`dark:text-[${COLOR_SCHEME.DARK_TEXT}] dark:bg-[${COLOR_SCHEME.DARK_BG}]`}
-      >
-        {children}
-      </div>
+      <div className={`dark:text-[#f1f1f1] dark:bg-[#141414]`}>{children}</div>
     </div>
   );
 };
